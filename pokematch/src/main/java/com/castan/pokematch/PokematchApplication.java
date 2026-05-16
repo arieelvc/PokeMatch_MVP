ad6346801d42bcb6737cd27af4fbb42b19c5b562
arieelvc <arielpvc17@gmail.com>
2026-05-16 22:40:16 +0200

    initial project setup with Spring Boot 4 and Java 21

---
+++ pokematch/src/main/java/com/castan/pokematch/PokematchApplication.java
@@ -0,0 +1,13 @@
+package com.castan.pokematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokematchApplication.class, args);
+	}
+
+}
---
+++ pokematch/src/test/java/com/castan/pokematch/PokematchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castan.pokematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
